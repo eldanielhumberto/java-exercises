10c4be7c76120d491422a5a891c2104b1201c03f
Humberto Daniel <gamekik92@gmail.com>
2025-05-09 22:55:54 -0600

    refactor: using ternary operator

--- two-fer/src/main/java/Twofer.java
+++ two-fer/src/main/java/Twofer.java
@@ -1,9 +1,5 @@
 public class Twofer {
     public String twofer(String name) {
-        if (name == null || name.isEmpty()) {
-            return "One for you, one for me.";
-        }
-
-        return "One for " + name + ", one for me.";
+        return name == null || name.isEmpty() ? "One for you, one for me." : "One for " + name + ", one for me.";
     }
 }
