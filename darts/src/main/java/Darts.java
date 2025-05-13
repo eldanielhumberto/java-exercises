edf1f64c323718435575c5970022c7eca775c07a
Humberto Daniel <gamekik92@gmail.com>
2025-05-13 10:16:59 -0600

    refactor: refactoring "darts"

--- darts/src/main/java/Darts.java
+++ darts/src/main/java/Darts.java
@@ -1,16 +1,16 @@
 class Darts {
     int score(double xOfDart, double yOfDart) {
         double d = Math.sqrt(Math.pow(xOfDart, 2) + Math.pow(yOfDart, 2));
-        if (d <= 10 && d > 5) {
-            return 1;
+        if (d <= 1) {
+            return 10;
         }
 
-        if (d <= 5 && d > 1) {
+        if (d <= 5) {
             return 5;
         }
 
-        if (d <= 1) {
-            return 10;
+        if (d <= 10) {
+            return 1;
         }
 
         return 0;
